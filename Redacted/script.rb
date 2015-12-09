f2d7a081f15721958b3bdf8bafcaaee73fec887c
Garrett Knox <gakf38@mail.missouri.edu>
2015-12-08 22:16:30 -0600

    Update script.rb

--- Redacted/script.rb
+++ Redacted/script.rb
@@ -1,1 +1,22 @@
+=begin
+Redacted!
+=end
 
+puts "Enter a string"
+text = gets.chomp
+words = text.split(" ")
+
+puts "Enter a word to redact"
+redact = gets.chomp
+
+redacted = "" 
+
+words.each { |word|
+    if word.downcase == redact.downcase
+        redacted += "REDACTED" + " "
+    else
+        redacted += word + " "
+    end
+}
+
+print redacted
