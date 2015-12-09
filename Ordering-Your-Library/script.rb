20182bebb1bc64452f64faf5963d1d9511550204
Garrett Knox <gakf38@mail.missouri.edu>
2015-12-09 13:37:10 -0600

    Update script.rb

--- Ordering-Your-Library/script.rb
+++ Ordering-Your-Library/script.rb
@@ -1,1 +1,15 @@
+=begin
+Learn Ruby: Ordering Your Library
+=end
 
+def alphabetize(arr, rev=false)
+    arr.sort!
+    if rev == true
+        return arr.reverse!
+    else
+        return arr
+    end
+end
+
+books = ["Harry Potter", "The Shining", "Lord of the Rings"]
+puts alphabetize(books, true)
