a9e57f99904cc5ad14f27da92fd9a2e72beea853
Garrett Knox <gakf38@mail.missouri.edu>
2015-12-08 23:00:51 -0600

    Update script.rb

--- Create-a-Histogram/script.rb
+++ Create-a-Histogram/script.rb
@@ -1,1 +1,16 @@
+=begin
+Create a Histogram
+=end
 
+puts "Enter a string"
+text = gets.chomp
+words = text.split(" ")
+
+frequencies = Hash.new(0)
+
+words.each { |word| frequencies[word] += 1 }
+
+frequencies = frequencies.sort_by { |word, count| count }
+frequencies.reverse!
+
+frequencies.each {|word, count| puts "#{word} #{count}"}
