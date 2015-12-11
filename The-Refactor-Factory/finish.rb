7a58a652a8dfc5ee379e3b5e0f4c508a2f8aaf92
Garrett Knox <gakf38@mail.missouri.edu>
2015-12-10 23:36:38 -0600

    Create finish.rb

---
+++ The-Refactor-Factory/finish.rb
@@ -0,0 +1,21 @@
+=begin
+Learn Ruby: The Refactor Factory
+=end
+
+$VERBOSE = nil    # We'll explain this at the end of the lesson.
+require 'prime'   # This is a module. We'll cover these soon!
+
+def first_n_primes(n)
+
+  "n must be an integer" unless n.is_a? Integer
+
+  return "n must be greater than 0." if n <= 0
+  
+  prime_array ||= []
+  
+  prime = Prime.new
+  n.times { prime_array << prime.next }
+  prime_array
+end
+
+first_n_primes(10)
